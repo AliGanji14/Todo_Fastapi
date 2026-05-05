096f8a09a7d2b5fcc87c0a32f2e43a1a6d61ab30
aliganji14 <aliganji142879@gmail.com>
2026-05-05 20:40:50 +0330

    working with CORS middleware

--- core/main.py
+++ core/main.py
@@ -60,10 +60,9 @@ def set_cookie(request: Request):
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
-    start_time = time()  # زمان شروع پردازش درخواست
-    response = await call_next(request)  # پردازش درخواست توسط FastAPI
-    process_time = time.perf_counter() - start_time  # مدت زمان پردازش محاسبه شود
-    # اضافه کردن هدر به پاسخ
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
     response.headers["X-Process-Time"] = str(process_time)
     return response
 
